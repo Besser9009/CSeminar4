7f1533a8a03e009fedca086b0ba65a1c095f3932
besser <besser9009@mail.ru>
2023-04-09 15:22:30 +0300

    add Test1 and Test2

---
+++ Test2/Program.cs
@@ -0,0 +1,18 @@
+﻿int SetNumber(string text = "") //45987
+{
+    System.Console.Write($"Enter number {text}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int MethodNum(int number)
+{
+    int count = 0;
+    if (number < 0) number *= -1;
+    for (; count <= number; count++)
+    {
+        number /= 10;
+    }
+    return count;
+}
+int number = SetNumber();
+Console.WriteLine($"Quantity number = {MethodNum(number)}");
